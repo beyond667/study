c3a2e04dc446c64aa4403d35f63190ecd7015540
paul.zhang <552499041@qq.com>
2019-05-20 22:29:04 +0800

    service中弹dialog

--- app/src/main/java/demo/beyond/com/blog/service/MyService.java
+++ app/src/main/java/demo/beyond/com/blog/service/MyService.java
@@ -1,10 +1,17 @@
 package demo.beyond.com.blog.service;
 
+import android.app.AlertDialog;
 import android.app.Service;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.SystemClock;
 import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MyService extends Service {
     public MyService() {
@@ -31,7 +38,36 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.e("==", "======onStartCommand==");
+//        Toast.makeText(this, "this is service", Toast.LENGTH_LONG).show();
         return START_STICKY;
+    }
+
+    private void showDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");
+        builder.setMessage("这里是service弹出的系统dialog");
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        final AlertDialog dialog = builder.create();
+        //在dialog  show方法之前添加如下代码，表示该dialog是一个系统的dialog
+        //8.0新特性
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY);
+        } else {
+            dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        }
+        dialog.show();
+
     }
 
     @Override
@@ -47,6 +83,6 @@ public class MyService extends Service {
     }
 
     public void myMethod() {
-        Log.e("==", "======myMethod==");
+        showDialog();
     }
 }
--- app/src/main/java/demo/beyond/com/blog/service/ServiceActivity.java
+++ app/src/main/java/demo/beyond/com/blog/service/ServiceActivity.java
@@ -3,13 +3,17 @@ package demo.beyond.com.blog.service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.PersistableBundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -22,12 +26,19 @@ public class ServiceActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-
-
         ButterKnife.bind(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(ServiceActivity.this)) {
+                //若没有权限，提示获取.
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                Toast.makeText(ServiceActivity.this,"需要取得权限以使用悬浮窗",Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        }
+
     }
     @OnClick({R.id.bt_start,R.id.bt_stop,R.id.bt_bind,R.id.bt_unbind,
-            R.id.bt_bind_important,R.id.bt_not_foreground,R.id.bt_debug_unbind})
+            R.id.bt_bind_important,})
     void clickView(View view) {
         Intent intent = new Intent(ServiceActivity.this,MyService.class);
         switch (view.getId()) {
@@ -42,15 +53,6 @@ public class ServiceActivity extends AppCompatActivity{
                 break;
             case R.id.bt_bind_important:
                 bindService(intent,connection,BIND_IMPORTANT);
-                break;
-            case R.id.bt_not_foreground:
-                bindService(intent,connection,BIND_NOT_FOREGROUND);
-                break;
-            case R.id.bt_debug_unbind:
-                bindService(intent,connection,BIND_DEBUG_UNBIND);
-                break;
-            case R.id.bt_unbind:
-                unbindService(connection);
                 break;
             default:
                 break;
